b0be6d7348e793cafc0e198e50c24305c4aae12a
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-06-15 09:01:02 -0300

    interpolacao_variaveis_string.py

---
+++ interpolacao_variaveis_string.py
@@ -0,0 +1,13 @@
+nome = "Darlan"
+idade= "22"
+profissao= "estudante"
+linguagem= "python"
+saldo = 98.533
+
+print (f"Ola, me chamdo {nome} e tenho {idade} anos de idade. Sou {profissao} e estou estudando a linguagem {linguagem} Tenho {saldo} de saldo negativo.")
+#Ola, me chamdo Darlan e tenho 22 anos de idade. Sou estudante e estou estudando a linguagem python. Tenho 98.533 de saldo negativo.
+
+print (f"Ola, me chamdo {nome} e tenho {idade} anos de idade. Sou {profissao} e estou estudando a linguagem {linguagem}. Tenho {saldo:.2f} de saldo negativo.")
+#Ola, me chamdo Darlan e tenho 22 anos de idade. Sou estudante e estou estudando a linguagem python. Tenho 98.53 de saldo negativo.
+#ou seja, quando eu coloco esse :2f (nao precisa ser 2) eu escolho quantos numeros tem apos o .
+
