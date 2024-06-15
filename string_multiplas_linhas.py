3316098977d399e62dbd0a8ad654a95bdc12ba96
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-06-15 09:18:59 -0300

    string_multiplas_linhas.py

---
+++ string_multiplas_linhas.py
@@ -0,0 +1,25 @@
+nome = "darlan"
+curso= "python"
+print(f"""Ola, me chamdo {nome}
+      e estrou estudando {curso}
+      """)
+
+#as aspas triplas respeitam o que eu coloquei no print, podendo ter varios recuos, por exemplo.
+
+print(f"""Ola, me     chamdo {nome}
+         e estrou estudando {curso}
+      """)
+
+#ele respeitas esses espacos que dei
+
+print("""
+      ============MENU===========
+      
+      1- SACAR
+      2- DEPOSITAR
+      3- EXTRATO
+      0- SAIR
+
+      ===========================
+      
+      """)
