a1b76997abde80516ecec146d935ec2257246d3b
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-06-15 09:12:16 -0300

    fatiamento_string.py

---
+++ fatiamento_string.py
@@ -0,0 +1,19 @@
+#lembrando que o 0 conta, ou seja, quando passar um numero sera ele -1 nas letras da frase.
+nome = "Jose Darlan De Lima Carvalho"
+#tem os principais numero. antes e dps dos :
+#o numeor antes dos : server para pular aquela quantidade de letras que tem no lado esquerdo e colocar o lado direito normal
+#e o numero dps dos : serve para pular o lado direito e colcoar o esquerdo
+
+
+
+print(nome[0])
+#pega a primeira letra
+print(nome[:5])
+print(nome[5:])
+print(nome[5:12])
+print(nome[12:15:1])
+#o terceiro numero é o step, ou seja, ele pula;
+print(nome[:])
+#exibe o nome normal e completo
+print(nome[::-1])
+#deixa o nome ao contrario "ohlavraC amiL eD nalraD esoJ"
